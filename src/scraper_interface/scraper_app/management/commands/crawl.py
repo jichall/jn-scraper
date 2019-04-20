c0714b15200dac1c6cbc0008f9646b325c4c26b7
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-04-19 21:08:33 -0300

    Move the crawler command to where it belongs
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/scraper_interface/scraper_app/management/commands/crawl.py
+++ src/scraper_interface/scraper_app/management/commands/crawl.py
@@ -1,8 +1,11 @@
-from django.core.management.base import BaseCommand
+from sys import path
 
-from scraper.spiders import ProductSpider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from django.core.management.base import BaseCommand
+
+path.append('../../../scraper/spiders/')
+from scraper.spiders import products_spider
 
 
 class Command(BaseCommand):
@@ -11,5 +14,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         process = CrawlerProcess(get_project_settings())
 
-        process.crawl(ProductSpider)
+        process.crawl(products_spider.ProductsSpider)
         process.start()
